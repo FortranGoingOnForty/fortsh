543872cffacf43db1130f311c2408a8032a5e725
espadonne <espadonne@outlook.com>
2026-03-09 19:38:21 -0400

    bump version to 1.3.2

--- src/common/version.f90
+++ src/common/version.f90
@@ -7,7 +7,7 @@ module version
   private
   public :: FORTSH_VERSION, print_version, print_help
 
-  character(len=*), parameter :: FORTSH_VERSION = "1.3.1"
+  character(len=*), parameter :: FORTSH_VERSION = "1.3.2"
 
 contains
 
