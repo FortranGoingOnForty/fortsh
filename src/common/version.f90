d415036293160f3ff17f71f2e2fd3a33ef4d92dd
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-29 22:48:06 -0400

    bump version to 1.8.0

--- src/common/version.f90
+++ src/common/version.f90
@@ -7,7 +7,7 @@ module version
   private
   public :: FORTSH_VERSION, print_version, print_help
 
-  character(len=*), parameter :: FORTSH_VERSION = "1.7.0"
+  character(len=*), parameter :: FORTSH_VERSION = "1.8.0"
 
 contains
 
